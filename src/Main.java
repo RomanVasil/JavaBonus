f365537511d9cc18f901bc845ec78808083ff932
RomanVasil <jostik85@outlook.com>
2022-04-20 14:12:22 +0400

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,17 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 100;               // баланс счета до пополнения
-        int minReplenishment=1000;       //  мин. сумма пополнения для начисления бонусов
+        int minReplenishment = 1000;       //  мин. сумма пополнения для начисления бонусов
         int bonusPrice = 100;            // цена бонуса от суммы пополнения
         int replenishment = 900;         // сумма пополнения
 
-        int bonus = replenishment/bonusPrice;
+        int bonus = replenishment / bonusPrice;
         if (replenishment > minReplenishment) {
-            System.out.println(replenishment/bonusPrice + " начисленно бонусных рублей");
-        }
-        else {
+            System.out.println(replenishment / bonusPrice + " начисленно бонусных рублей");
+        } else {
             bonus = 0;
         }
 
-        System.out.println(balance+replenishment+bonus + " баланс счета");
+        System.out.println(balance + replenishment + bonus + " баланс счета");
     }
 }
